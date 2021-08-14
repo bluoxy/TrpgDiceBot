4bf30aa510fa3bada59fbc30cf533198bb8062f1
BluOxy <bluelightbleze@gmail.com>
2021-08-15 00:01:51 +0900

    add ccb command

---
+++ TrpgDiceBot/Commands/CallOfCthulhu.cs
@@ -0,0 +1,55 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrpgDiceBot.Commands
+{
+    public class CallOfCthulhu : ModuleBase
+    {
+        private readonly static Random _random = new Random();
+
+        /// <summary>
+        /// ヘルプを表示します
+        /// </summary>
+        /// <returns></returns>
+        [Command("ccb")]
+        public async Task Battle()
+        {
+            var rollResult = _random.Next(1, 101);
+            await ReplyAsync($"(1D100) → {rollResult}");
+        }
+        /// <summary>
+        /// ヘルプを表示します
+        /// </summary>
+        /// <returns></returns>
+        [Command("ccb")]
+        public async Task Battle(int skillValue)
+        {
+            var rollResult = _random.Next(1, 101);
+            var skillResult = "失敗";
+            if (rollResult <= skillValue)
+            {
+                if (rollResult <= 5　&& rollResult <= skillValue / 5)
+                {
+                    skillResult = "決定的成功/スペシャル";
+                }
+                else if (rollResult <= skillValue / 5)
+                {
+                    skillResult = "スペシャル";
+                }
+                else
+                {
+                    skillResult = "成功";
+                }
+            }
+            else if(rollResult > 95)
+            {
+                skillResult = "致命的失敗";
+            }
+
+            await ReplyAsync($"(1D100<={skillValue}) → {rollResult} → {skillResult}");
+        }
+    }
+}
---
+++ TrpgDiceBot/Commands/Help.cs
@@ -0,0 +1,29 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrpgDiceBot
+{
+    public class Help : ModuleBase
+    {
+        /// <summary>
+        /// ヘルプを表示します
+        /// </summary>
+        /// <returns></returns>
+        [Command("help")]
+        public async Task Display()
+        {
+            var helps = new []
+            {
+                "XdY … Y面ダイスをX個振ります",
+                "1d6 … 6面ダイスを1個振ります",
+                "ZrXdY … Z回Y面ダイスをX個振ります",
+                "3r1d6 … 3回6面ダイスを1個振ります",
+                "!ping … pong"
+            };
+            await ReplyAsync(string.Join(Environment.NewLine, helps));
+        }
+    }
+}
